5f3ff490781946605b7cafc5a23348db3f251172
hsnendheje-cmd <hsnendheje@gmail.com>
2026-08-20 04:18:41 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,5 @@
 @echo off
 cd /d "%~dp0"
 title Toast Menu [PAID]
-cls
-echo.
-echo hold up...
-echo.
 frida -l ac_bridge.js -l toastmenu.js "EACLauncher.exe"
-
-echo.
-echo just press anything
-pause > nul
+pause
