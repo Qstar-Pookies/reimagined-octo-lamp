c3e87e02889edfc4e1c6b1cb633ddcaed2fbaf34
hsnendheje-cmd <hsnendheje@gmail.com>
2026-08-26 04:28:28 -0400

    Update start.bat to clear screen and suppress output

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-title Toast Menu [PAID]
-frida -l ac_bridge.js -l toastmenu.js "EACLauncher.exe"
-pause
+title Toasts Menu [PAID]
+cls
+frida -l ac_bridge.js -l toastmenu.js "EACLauncher.exe" >nul 2>&1
